7750af5c0ea3a29fda11cbf7d163fdba94012775
tamani_karim50 <tamanikarim50@gmail.com>
2023-05-20 11:27:39 -0700

    search hashtags

--- src/components/Cards/search/SearchHashTags.js
+++ src/components/Cards/search/SearchHashTags.js
@@ -1,19 +1,165 @@
-import {View , Text , StyleSheet} from "react-native" ; 
+import { useCallback, useContext, useEffect, useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { ApolloContext } from "../../../providers/ApolloContext";
+import { FlatList } from "react-native-gesture-handler";
+import LoadingActivity from "../post/loadingActivity";
+import { textFonts } from "../../../design-system/font";
+import { gql } from "@apollo/client";
+
+const LIMIT = 10 ;
+export default function SearchHashTags({ query , navigation }) {
+    const client = useContext(ApolloContext);
+    const [hashtags, setHashtags] = useState([]);
 
 
-export default function SearchHashTags({query }) { 
+    const [firstFetch, setFirstFetch] = useState(true);
+    const [loading, setLoading] = useState(false);
+    const [end, setEnd] = useState(false);
 
-    return(
-        <View style = {styles.container} >
-<Text>Search Hashtags </Text>
+    const search_hashtags = async (query, previousSearch) => {
+        var offset = previousSearch.length;
+        client.query({
+            query: gql`
+        
+            query SearchHashTag($name: String!, $offset: Int!, $limit: Int!) {
+                searchHashTag(name: $name, offset: $offset, limit: $limit) {
+                    id name numPosts 
+                }
+            }
+          ` ,
+            variables: {
+                name: query,
+                offset,
+                limit: LIMIT
+            }
+        }).then(response => {
+
+            var newSearch = response.data.searchHashTag;
+            setHashtags([...previousSearch, ...newSearch])
+
+            if (newSearch.length < LIMIT)
+                setEnd(true);
+
+            setLoading(false);
+            setFirstFetch(false);
+
+        }).catch(error => {
+            setLoading(false);
+            setFirstFetch(false);
+        })
+
+    }
+
+    useEffect(() => {
+
+        setFirstFetch(true) ; 
+        setEnd(false) ; 
+        setLoading(false) ; 
+        if (query)
+            search_hashtags(query, [])
+        else
+        search_hashtags("", []);
+    }, [query]);
+
+
+    useEffect(() => {
+        if (loading)
+            search_hashtags(query, hashtags.filter(hashtag => hashtag.type != "loading") );
+    }, [loading])
+
+   
+
+    const openHashtag = useCallback((hashtag) => {
+        navigation.navigate("HashTag" , {
+            hashtagName :  hashtag
+        })
+    } , [navigation]) 
+
+
+
+    const renderItem = useCallback(({ item, index }) => {
+        if (item.type == "loading") {
+            return <LoadingActivity style={{ height: 56 }} />
+        }
+        return (
+            <TouchableOpacity style={styles.hashResult} onPress={() => openHashtag(item.name)}>
+                <Text style={[styles.hashtag, styles.hashtagName]}>
+                    {item.name}
+                </Text>
+                <Text style={styles.numPosts}>
+                    عدد المنشورات {item.numPosts}
+                </Text>
+            </TouchableOpacity>
+        )
+
+    }, [navigation ]);
+
+
+
+
+    const keyExtractor = useCallback((item, index) => {
+        return item.id;
+    }, []) ; 
+
+
+    const reachEnd = useCallback(() => {
+
+        if (!loading && !end && !firstFetch) {
+            setHashtags([...hashtags, { id: 0, type: "loading" }])
+            setLoading(true);
+        }
+    }, [loading, hashtags , end, firstFetch]);
+
+
+    return (
+        <View style={styles.container} >
+         {
+                !firstFetch &&
+                <FlatList
+                    data={hashtags}
+                    keyExtractor={keyExtractor}
+                    renderItem={renderItem}
+                    style={styles.list}
+                    onEndReached={reachEnd}
+                    onEndReachedThreshold={0.5}
+                    ItemSeparatorComponent={<View style={styles.separator}></View>}
+                />
+            }
+            {
+                firstFetch &&
+                <LoadingActivity />
+            }
         </View>
     )
 
 
-} ; 
+};
 
 const styles = StyleSheet.create({
-    container : {
-        flex : 1
-    }
+    container: {
+        flex: 1
+    } , 
+    
+    
+    hashResult: {
+        paddingVertical: 4,
+        paddingHorizontal : 16 , 
+        alignItems: "flex-end"
+    },
+    hashtagName: {
+        fontFamily: textFonts.semiBold,
+        fontSize: 12
+    },
+    numPosts: {
+        color: "#888",
+        fontFamily: textFonts.regular,
+        fontSize: 10
+    },
+    separator: {
+        height: 1,
+        backgroundColor: "#eee"
+    } , 
+    hashtag: {
+        color: "#1A6ED8"
+    },
 })
